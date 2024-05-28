4f1af7ea75496c1d34c127b67f792beab9c11a93
Hasanur Rahman <105588130+hasanur432@users.noreply.github.com>
2024-05-28 16:58:26 +0530

    check

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,15 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://deme1.netlify.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/profile", express.static("upload/images"));
 app.use(express.json());
 app.use("/", Routes);
