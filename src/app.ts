f6f41022d53adcf2268af52e8a0599c43f311679
Hasanur Rahman <105588130+hasanur432@users.noreply.github.com>
2024-05-29 12:50:23 +0530

    check

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ app.use(
     origin: "https://deme1.netlify.app",
   })
 );
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://deme1.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,14 +2,15 @@ import mongoose from "mongoose";
 
 const db = () => {
   const url =
-    "mongodb+srv://rhasanur234:X4gg8PGH1FbOEKQ7@cluster0.ochoajc.mongodb.net/";
+    "mongodb+srv://rhasanur234:2zZIvzXz76j1GpyT@cluster0.ochoajc.mongodb.net/demo?retryWrites=true&w=majority";
+
   mongoose
-    .connect(url)
+    .connect(url, { serverSelectionTimeoutMS: 5000 }) // Adjust the timeout as needed
     .then(() => {
       console.log("Database is connected");
     })
     .catch((e: Error) => {
-      console.log("Database is not connected ", e);
+      console.log("Database is not connected", e);
     });
 };
 
